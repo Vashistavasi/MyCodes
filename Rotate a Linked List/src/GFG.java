1e2ba0656102bbee0c7f33693d5a33760f806561
10683788 <10683788@LTILMAA21200704.lntinfotech.com>
2022-07-25 09:11:44 +0530

    Rotating a Linked List

---
+++ Rotate a Linked List/src/GFG.java
@@ -0,0 +1,93 @@
+//{ Driver Code Starts
+import java.util.*;
+class Node {
+    int data;
+    Node next;
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+} 
+
+class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            
+            int a = sc.nextInt();
+            Node head = new Node(a);
+            Node tail = head;
+            
+            for (int i=0; i<n-1; i++)
+            {
+                a = sc.nextInt();
+                tail.next = new Node(a);
+                tail = tail.next;
+            }
+            
+            int k = sc.nextInt();
+            
+            Solution ob = new Solution();
+            head = ob.rotate(head,k);
+            printList(head);
+        }
+    }
+    
+    public static void printList(Node n) {
+        while (n != null) {
+            System.out.print(n.data + " ");
+            n = n.next;
+        }
+        System.out.println();
+    }
+}
+
+// } Driver Code Ends
+
+
+/* node of linked list:
+
+class Node{
+    int data;
+    Node next;
+    Node(int d){
+        data=d;
+        next=null;
+    }
+}
+
+*/
+
+class Solution{
+    //Function to rotate a linked list.
+    	public static Node ro(Node head) {
+		Node current=head;
+		Node previous=new Node(0);
+		while(current.next!=null) {
+		    previous=current;
+			current=current.next;
+		}
+		previous.next=null;
+		current.next=head;
+		return current;
+		
+	}
+    public Node rotate(Node head, int k) {
+        // add code here
+        int count=0;
+        Node current=head;
+        while(current!=null){
+            current=current.next;
+            count++;
+        }
+    
+    	for(int i=0;i<count-k;i++) {
+    		head=ro(head);
+    	}
+    	return head;	
+    }
+}
