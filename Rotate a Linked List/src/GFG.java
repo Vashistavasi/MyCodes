428e1d0dfe55f90fbba0d16a5b1ce56184ab50b1
10683788 <10683788@LTILMAA21200704.lntinfotech.com>
2022-07-25 09:21:17 +0530

    Reversing a Linked List

--- Rotate a Linked List/src/GFG.java
+++ Rotate a Linked List/src/GFG.java
@@ -1,93 +1,93 @@
+//{ Driver Code Starts
 import java.util.*;
-import java.io.*;
+class Node {
+    int data;
+    Node next;
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+} 
+
+class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            
+            int a = sc.nextInt();
+            Node head = new Node(a);
+            Node tail = head;
+            
+            for (int i=0; i<n-1; i++)
+            {
+                a = sc.nextInt();
+                tail.next = new Node(a);
+                tail = tail.next;
+            }
+            
+            int k = sc.nextInt();
+            
+            Solution ob = new Solution();
+            head = ob.rotate(head,k);
+            printList(head);
+        }
+    }
+    
+    public static void printList(Node n) {
+        while (n != null) {
+            System.out.print(n.data + " ");
+            n = n.next;
+        }
+        System.out.println();
+    }
+}
+
+// } Driver Code Ends
+
+
+/* node of linked list:
 
 class Node{
     int data;
     Node next;
-    
-    Node(int x){
-        data = x;
-        next = null;
+    Node(int d){
+        data=d;
+        next=null;
     }
-    
 }
-class GFG{
-	static void printList(Node node) 
-	{ 
-		while (node != null) 
-		{ 
-			System.out.print(node.data + " "); 
-			node = node.next; 
-		} 
-		System.out.println(); 
-	}
-    public static void main(String args[]) throws IOException { 
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        while(t > 0){
-        	int n = sc.nextInt();
-            Node head = new Node(sc.nextInt());
-            Node tail = head;
-            for(int i=0; i<n-1; i++)
-            {
-                tail.next = new Node(sc.nextInt());
-                tail = tail.next;
-            }
-            Solution g = new Solution();
-            int ans = g.getMiddle(head);
-            System.out.println(ans);
-            //printList(head); 
-            t--;
-        }
-    } 
-} 
-   
-// } Driver Code Ends
 
-
-/* Node of a linked list
- class Node {
-   int data;
-    Node next;
-    Node(int d)  { data = d;  next = null; }
-}
 */
 
-class Solution
-{
-    public static int count(Node head){
-        int n=0;
-        while(head.next!=null){
-            n++;
-            head=head.next;
+class Solution{
+    //Function to rotate a linked list.
+    	public static Node ro(Node head) {
+		Node current=head;
+		Node previous=new Node(0);
+		while(current.next!=null) {
+		    previous=current;
+			current=current.next;
+		}
+		previous.next=null;
+		current.next=head;
+		return current;
+		
+	}
+    public Node rotate(Node head, int k) {
+        // add code here
+        int count=0;
+        Node current=head;
+        while(current!=null){
+            current=current.next;
+            count++;
         }
-        return n+1;
-    }
-    int getMiddle(Node head)
-    {
-         // Your code here.
-         int n=count(head);
-         //System.out.println(n);
-         int ans=0;
-         int value=0;
-        if(n%2==0) {
-             int element=(n/2)+1;
-             while(element!=ans){
-                 ans++;
-                 value=head.data;
-                 head=head.next;
-             }
-             return value;
-         }
-         else{
-             int element=n/2;
-            while(element!=ans){
-                 ans++;
-                 head=head.next;
-             }
-             return head.data;
-         }
-         
-         
+    
+    	for(int i=0;i<count-k;i++) {
+    		head=ro(head);
+    	}
+    	return head;	
     }
 }
