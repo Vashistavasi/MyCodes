2507174076650d585afc83f4f719331fdb39bae8
10683788 <10683788@LTILMAA21200704.lntinfotech.com>
2022-07-31 20:55:39 +0530

    Easy

---
+++ Minimum Operations/src/GFG.java
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main(String args[])
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    int n = sc.nextInt();
+                    Solution ob = new Solution();
+                    System.out.println(ob.minOperation(n));
+                }
+        }
+}    
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution
+{
+    public int minOperation(int n)
+    {
+        //code here.
+        if(n==0)
+        return 0;
+        int modulo=n%2;
+        if(modulo==0)
+        return 1+minOperation(n/2);
+        else
+        return 1+minOperation(n-1);
+        
+    }
+}
