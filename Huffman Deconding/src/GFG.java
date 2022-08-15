e21dd55b98a1a472a3e25118ebc0f0c1c1299018
10683788 <10683788@LTILMAA21200704.lntinfotech.com>
2022-08-15 18:07:51 +0530

    Weird Decoding for single character

---
+++ Huffman Deconding/src/GFG.java
@@ -0,0 +1,128 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+class minHeapNode {
+    char data;
+    int freq;
+    minHeapNode left, right;
+    minHeapNode(char data, int freq){
+        left = right = null;
+        this.data = data;
+        this.freq = freq;
+    }
+}
+
+class heapComparator implements Comparator<minHeapNode> {
+    public int compare(minHeapNode h1, minHeapNode h2) { 
+        if (h1.freq < h2.freq) {
+            return 1;
+        } else if (h1.freq > h2.freq) {
+            return -1;
+        }
+        return 0; 
+    } 
+} 
+
+class driverClass {
+    
+    static PriorityQueue<minHeapNode> heapq;
+    static HashMap<Character, Integer> freqMap;
+    static HashMap<Character, String> codes;
+    
+    static void storeCodes(minHeapNode root, String str) {
+        if(root==null)return;
+        if(root.data != '$')codes.put(root.data, str);
+        storeCodes(root.left, str + "0");
+        storeCodes(root.right, str + "1");
+    }
+    
+    static void encodeString(){
+        minHeapNode left, right, top;
+        for (Map.Entry<Character, Integer> val:freqMap.entrySet()) {
+            heapq.add(new minHeapNode(val.getKey(), val.getValue()));
+        }
+        while (heapq.size() != 1) {
+            left = heapq.poll();
+            right = heapq.poll();
+            top = new minHeapNode('$', left.freq + right.freq);
+            top.left = left;
+            top.right = right;
+            heapq.add(top);
+        }
+        storeCodes(heapq.peek(), "");
+    }
+    
+    static void calcFreq(String str) {
+        for (int i=0; i<str.length(); i++) { 
+            Integer j = freqMap.get(str.charAt(i)); 
+            freqMap.put(str.charAt(i), (j == null) ? 1 : j + 1); 
+        } 
+    }
+	
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim());
+		while(t-->0){
+            String str = br.readLine().trim();
+            heapq = new PriorityQueue<minHeapNode>(new heapComparator());
+            codes = new HashMap<Character, String>();
+            freqMap = new HashMap<Character, Integer>();
+            String encodedString = "";
+            calcFreq(str);
+            encodeString();
+            for(int i=0; i<str.length(); i++){
+                encodedString+=codes.get(str.charAt(i));
+            }
+            GFG g = new GFG();
+            System.out.println(g.decodeString(heapq.peek(), encodedString));
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+class GFG {
+    // your required to complete this function
+    // this function should return the encoded string
+    // see the main function for the minHeapNode reference
+	
+	
+	
+	
+    String decodeString(minHeapNode root, String encodedStr){
+        // Code here
+        minHeapNode temp=root;
+        String s="";
+        for(int i=0;i<=encodedStr.length();i++){
+            //System.out.println(encodedStr.charAt(i));
+            if(root.left==null && root.right==null ){
+                s+=root.data;
+                root=temp;
+                i--;
+                if(i==-1)
+                	break;
+               // System.out.println("Reset "+ s);
+                
+            }
+        else if(i<encodedStr.length() && encodedStr.charAt(i)=='0'){
+                
+                root=root.left;
+              //  System.out.println("left");
+            }
+            else{if(i<encodedStr.length() )
+                root=root.right;
+               // System.out.println("right");
+            }
+        }
+       // System.out.println(temp.left.left.data);
+      // System.out.println(temp.right.data);
+        return s;
+    }
+}
+
