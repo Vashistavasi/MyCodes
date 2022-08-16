47717e0b514e6be43a4783f9874a625fc354fe90
10683788 <10683788@LTILMAA21200704.lntinfotech.com>
2022-08-16 08:18:42 +0530

    Left View of Binary Tree

---
+++ Left View of Binary Tree/src/GFG.java
@@ -0,0 +1,167 @@
+//{ Driver Code Starts
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GfG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root)
+    {
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+        	    Tree g = new Tree();
+    			ArrayList<Integer> result = g.leftView(root);
+    			for(int value : result){
+    			    System.out.print(value + " ");
+    			}
+    			System.out.println();
+                t--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/* A Binary Tree node
+class Node
+{
+    int data;
+    Node left, right;
+
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}*/
+class Tree
+{
+    //Function to return list containing elements of left view of binary tree.
+    static int max;
+    public static void level(Node root, int lev, ArrayList<Integer> ans){
+        if(root==null)
+        return ;
+        if(max<lev)
+        {
+            ans.add(root.data);
+            max=lev;
+            //System.out.println(ans.toString());
+        }
+        level(root.left,lev+1,ans);
+        level(root.right,lev+1,ans);
+    }
+    	public static ArrayList<Integer> left(Node root, ArrayList<Integer> ans){
+		if(root==null)
+			return ans;
+		else if(root.left!=null) {
+			ans.add(root.data);
+			root=root.left;
+			return left(root,ans);
+		}
+		else if(root.right!=null) {
+			ans.add(root.data);
+			root=root.right;
+			return left(root,ans);
+		}
+		else {
+			ans.add(root.data);
+			return ans;
+		}
+	}
+    ArrayList<Integer> leftView(Node root)
+    {
+      // Your code here
+    	ArrayList<Integer> ans=new ArrayList<Integer> ();
+    	max=0;
+    	level(root,1,ans);
+    	return ans;
+    	//return left(root,ans);
+
+    }
+}
